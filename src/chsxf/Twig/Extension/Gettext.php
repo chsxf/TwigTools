9ca1ee52de5b0d81b0886cc8fbc24c17b12d5d96
Christophe SAUVEUR <chsxf.pro@gmail.com>
2021-09-19 16:06:14 +0200

    ♻️ Refactored Gettext extension

--- src/chsxf/Twig/Extension/Gettext.php
+++ src/chsxf/Twig/Extension/Gettext.php
@@ -32,85 +32,19 @@ namespace chsxf\Twig\Extension {
         public function getFunctions()
         {
             return array(
-                    new TwigFunction('gettext', array(&$this, 'gettext')),
-                    new TwigFunction('_', array(&$this, 'gettext')),
-                    new TwigFunction('ngettext', array(&$this, 'ngettext')),
-                    new TwigFunction('_n', array(&$this, 'ngettext')),
-                    new TwigFunction('dgettext', array(&$this, 'dgettext')),
-                    new TwigFunction('_d', array(&$this, 'dgettext')),
-                    new TwigFunction('dngettext', array(&$this, 'dngettext')),
-                    new TwigFunction('_dn', array(&$this, 'dngettext'))
+                    new TwigFunction('gettext', 'gettext'),
+                    new TwigFunction('_', 'gettext'),
+                    new TwigFunction('ngettext', 'ngettext'),
+                    new TwigFunction('_n', 'ngettext'),
+                    new TwigFunction('dgettext', 'dgettext'),
+                    new TwigFunction('_d', 'dgettext'),
+                    new TwigFunction('dngettext', 'dngettext'),
+                    new TwigFunction('_dn', 'dngettext'),
+                    new TwigFunction('dcgettext', 'dcgettext'),
+                    new TwigFunction('_dc', 'dcgettext'),
+                    new TwigFunction('dcngettext', 'dcngettext'),
+                    new TwigFunction('_dcn', 'dcngettext')
             );
-        }
-        
-        /**
-         * Returns a localized message
-         *
-         * This function uses the native gettext() function to retrieve the message.
-         *
-         * @param string $msgid Message id
-         * @return string
-         *
-         * @link http://php.net/manual/en/function.gettext.php
-         * @link http://php.net/manual/en/function.sprintf.php
-         */
-        public function gettext($msgid)
-        {
-            return gettext($msgid);
-        }
-        
-        /**
-         * Returns a localized message from a specific domain
-         *
-         * This function uses the native dgettext() function to retrieve the message.
-         *
-         * @param string $domain Message domain
-         * @param string $msgid Message id
-         * @return string
-         *
-         * @link http://php.net/manual/en/function.dgettext.php
-         * @link http://php.net/manual/en/function.sprintf.php
-         */
-        public function dgettext($domain, $msgid)
-        {
-            return dgettext($domain, $msgid);
-        }
-        
-        /**
-         * Returns a string based on plural forms of the message
-         *
-         * This function uses the native ngettext() function to retrieve the message.
-         *
-         * @param string $msgid1 Singular form message id
-         * @param string $msgid2 Plural form message id
-         * @param int $n Number of units to consider
-         * @return string
-         *
-         * @link http://php.net/manual/en/function.ngettext.php
-         * @link http://php.net/manual/en/function.sprintf.php
-         */
-        public function ngettext($msgid1, $msgid2, $n)
-        {
-            return ngettext($msgid1, $msgid2, $n);
-        }
-        
-        /**
-         * Returns a string based on plural forms of the message from a specific domain
-         *
-         * This function uses the native dngettext() function to retrieve the message.
-         *
-         * @param string $domain Message domaine
-         * @param string $msgid1 Singular form message id
-         * @param string $msgid2 Plural form message id
-         * @param int $n Number of units to consider
-         * @return string
-         *
-         * @link http://php.net/manual/en/function.dngettext.php
-         * @link http://php.net/manual/en/function.sprintf.php
-         */
-        public function dngettext($domain, $msgid1, $msgid2, $n)
-        {
-            return dngettext($domain, $msgid1, $msgid2, $n);
         }
     }
 }
